79fbe417e91e2cd5a8720ad9d9f07b688fa31f7f
luoq <luoq08@gmail.com>
2011-11-01 14:02:38 +0800

    move convertion from graph to bigraph out of class,working on bipartite_ofaffian

--- polya.py
+++ polya.py
@@ -1,18 +1,43 @@
-from graph_tool import Graph
+from graph_tool.all import *
 class BiGraph(Graph):
     def __init__(self):
-        """ number of vertices in color class A """
+        """Number of vertices in color class A."""
         super(BiGraph,self).__init__(directed=False) #Note base class must be inited
-        self.num_A=0
         self.vertex_name=self.new_vertex_property('object')
-    def from_graph(self,G):
-        self.num_A=G.num_vertices()
-        self.clear()
-        self.add_vertex(2*self.num_A)
-        for i,j in G.edges():
-            self.add_edge(self.vertex(i),self.vertex(int(j)+self.num_A))
-            self.add_edge(self.vertex(j),self.vertex(int(i)+self.num_A))
+        self.num_A=0
+    def __init__(self,num_A,n):
+        super(BiGraph,self).__init__(directed=False) #Note base class must be inited
+        self.vertex_name=self.new_vertex_property('object')
+        self.num_A=num_A
+        self.add_vertex(n)
         for i in range(self.num_A):
             self.vertex_name[self.vertex(i)]='A'+str(i)
         for i in range(self.num_A,2*self.num_A):
             self.vertex_name[self.vertex(i)]='B'+str(i-self.num_A)
+        
+    def permute(self,P):
+        B=BiGraph()
+        B.num_A=self.num_A
+        B.add_vertex(self.num_vertices())
+        for i,j in self.vertices():#assume i is in A and j is in B
+            pass
+def graph_to_bigraph(G):
+    B=BiGraph(G.num_vertices(),2*G.num_vertices())
+    for i,j in G.edges():
+        B.add_edge(B.vertex(i),B.vertex(int(j)+B.num_A))
+        B.add_edge(B.vertex(j),B.vertex(int(i)+B.num_A))
+    return B
+    
+def bipartite_pfaffian(G):
+    """Check if bipartite graph G has a Pfaffian orientation"""
+    M,_=max_cardinality_matching(G)
+    permutation=range(G.num_A)
+    for i in range(G.num_A):
+        for e in G.vertex(i).out_edges():
+            if M[e]:
+                permutation[i]=int(e.target())
+                break
+        else:
+            print "No perfect mathing"
+            return False
+    return permutation
--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 import scipy.io
-import polya
-BiGraph=polya.BiGraph
+from polya import *
 from graph_tool.all import *
 from util import *
 F30=scipy.io.loadmat('../matrix/F30.mat')['F30']
 G=sparse_to_graph(F30)
 #graph_draw(G,size=(20,20),vsize=0.5,vprops={'label':G.vertex_index,'fixedsize':True},output='G30.png')
-BG=BiGraph()
-BG.from_graph(G)
-#graph_draw(BG,size=(20,20),vsize=0.5,vprops={'label':BG.vertex_name,'fixedsize':True},output='BG30.png')
+BG=graph_to_bigraph(G)
+graph_draw(BG,size=(20,20),vsize=0.5,vprops={'label':BG.vertex_name,'fixedsize':True},output='BG30.png')
