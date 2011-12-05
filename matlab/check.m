db03378463cffd71380791100fedf9ab45cae803
luoq <luoq08@gmail.com>
2011-12-05 16:33:04 +0800

    Add graph plot

--- matlab/check.m
+++ matlab/check.m
@@ -1,6 +1,9 @@
 function check
-A=grid_graph(8,8);
-if abs(sqrt(det(planar_pfaffian(A)))-12988816)>.1
+[A xy]=grid_graph(8,8);
+pf=planar_pfaffian(A);
+figure
+gplot(triu(pf)==1,xy,'r');hold on;gplot(triu(pf)==-1,xy,'b');hold off
+if abs(sqrt(det(pf))-12988816)>1e-3
     error('planar_pfaffian fail!')
 end
 disp('All Right')
