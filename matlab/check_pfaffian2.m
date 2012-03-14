7b89c9d2500ab265120ae9da4f89870c4566489a
luoq <luoq08@gmail.com>
2012-03-14 16:28:51 +0800

    clean check_pfaffian2.increase the thresold for test equaility
    1e-12 fails some tests

--- matlab/check_pfaffian2.m
+++ matlab/check_pfaffian2.m
@@ -1,48 +1,54 @@
-function check_pfaffian2
-k=5;
-%% test 1
-A=generate_matrix1(k);
-P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
-perm=Heperm(A);
-fprintf('test 1. perm=%d',perm)
-if abs(perm-abs(det(P)))<1e-12
-    disp(':-)')
+function check_pfaffian2(id,k)
+if id==1
+    %% test 1
+    A=generate_matrix1(k);
+    P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
+    perm=Heperm(A);
+    fprintf('test 1. perm=%d',perm)
+    if abs(perm-abs(det(P)))<1e-12
+        disp(':-)')
+    else
+        disp(':-(')
+    end
+elseif id==2
+    %% test 2
+    A=generate_matrix1(k);
+    A(2*k+1,2*k+1)=0;
+    P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
+    perm=Heperm(A);
+    fprintf('test 2. perm=%d',perm)
+    if abs(perm-abs(det(P)))<1e-12
+        disp(':-)')
+    else
+        disp(':-(')
+    end
+elseif id==3
+    %% test 3
+    A=generate_matrix1(k);
+    A(2*k+1,2*k+1)=0;
+    A(2*k+1,2*k+2)=0;
+    P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
+    perm=Heperm(A);
+    fprintf('test 3. perm=%d',perm)
+    if abs(perm-abs(det(P)))<1e-12
+        disp(':-)')
+    else
+        disp(':-(')
+    end
+    %% test 4
 else
-    disp(':-(')
-end
-%% test 2
-A(2*k+1,2*k+1)=0;
-P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
-perm=Heperm(A);
-fprintf('test 2. perm=%d',perm)
-if abs(perm-abs(det(P)))<1e-12
-    disp(':-)')
-else
-    disp(':-(')
-end
-%% test 3
-A(2*k+1,2*k+2)=0;
-P=bipartite_pfaffian(A(randperm(2*k+2),randperm(2*k+2)));
-perm=Heperm(A);
-fprintf('test 3. perm=%d',perm)
-if abs(perm-abs(det(P)))<1e-12
-    disp(':-)')
-else
-    disp(':-(')
-end
-%% test 4
-k=3;
-X1=generate_matrix1(k);
-X2=X1;
-A=glue(X1,[1 2*k+1;1 2*k+1],X2,[1 2*k+1;1 2*k+1]);
-%P=bipartite_pfaffian(A(randperm(size(A,1)),randperm(size(A,1))));
-P=bipartite_pfaffian(A);
-perm=Heperm(A);
-fprintf('test 4. perm=%d',perm)
-if abs(perm-abs(det(P)))<1e-12
-    disp(':-)')
-else
-    disp(':-(')
+    X1=generate_matrix1(k);
+    X2=X1;
+    A=glue(X1,[1 2*k+1;1 2*k+1],X2,[1 2*k+1;1 2*k+1]);
+    %P=bipartite_pfaffian(A(randperm(size(A,1)),randperm(size(A,1))));
+    P=bipartite_pfaffian(A);
+    perm=Heperm(A);
+    fprintf('test 4. perm=%d',perm)
+    if abs(perm-abs(det(P)))<1e-8
+        disp(':-)')
+    else
+        disp(':-(')
+    end
 end
 end
 
